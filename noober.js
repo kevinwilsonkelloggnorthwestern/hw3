152a628b48fc848b3145805ff0f6df71a782066c
Kevin Wilson <kevin.wilson@kellogg.northwestern.edu>
2021-04-22 08:56:07 -0500

    change1

--- noober.js
+++ noober.js
@@ -13,5 +13,36 @@ document.addEventListener('DOMContentLoaded', async function() {
   // 🔥 YOUR CODE GOES HERE 🔥
   // Write the recipe (algorithm) in the comments. Then, write the code.
   
+  // Create variable names for the data needed for the console log.
+  let passengerFirstName = ride.passengerDetails.first
+  let passengerLastName = ride.passengerDetails.last
+  let passengerPhoneNumber = ride.passengerDetails.phoneNumber
+  let pickupLocationStreet = ride.pickupLocation.address
+  let pickupLocationCity = ride.pickupLocation.city
+  let pickupLocationState = ride.pickupLocation.state
+  let pickupLocationZip = ride.pickupLocation.zip
+  let dropoffLocationStreet = ride.dropoffLocation.address
+  let dropoffLocationCity = ride.dropoffLocation.city
+  let dropoffLocationState = ride.dropoffLocation.state
+  let dropoffLocationZip = ride.dropoffLocation.zip
+  let purplerequested = ride.purpleRequested
+  let cartype
+  let passengerNumber = ride.numberOfPassengers
+  
+
+  // Create conditional such that the car type is always purple if requested, is XL if passengers is over 3 and purple isn't selected, and is X if none apply
+  if(purplerequested == true){
+  cartype = `Noober Purple`  
+  } else if(passengerNumber > 3){
+    cartype = `Noober XL`
+  } else {
+    cartype = `Noober X`
+  }
+
+  // Write out the ride description to the console
+  console.log(`${cartype} Passenger: ${passengerFirstName} ${passengerLastName} - ${passengerPhoneNumber}. Pickup at ${pickupLocationStreet}, ${pickupLocationCity}, ${pickupLocationState} ${pickupLocationZip}. Dropoff at ${dropoffLocationStreet}, ${dropoffLocationCity}, ${dropoffLocationState} ${dropoffLocationZip}`)
+  
+
+
   // 🔥 YOUR CODE ENDS HERE 🔥
 })
